577d0a6112d33c045b510dec99567bbc6dcf50c0
mitulgarg <mitulgarg3@gmail.com>
2025-04-06 18:11:56 +0530

    specify desired model name in main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import argparse
 from evaluator import SpeculativeDecoderEvaluator
 from plotter import ResultPlotter
 
 def main():
+    parser = argparse.ArgumentParser(description="Run speculative decoding analysis.")
+    parser.add_argument(
+        "--model", type=str, default="phi-3-mini-4k-instruct",
+        help="Base model name (without -8bit/-4bit suffixes)"
+    )
+    args = parser.parse_args()
+
     num_draft_tokens_list = [0, 1, 2, 3, 4]
-    evaluator = SpeculativeDecoderEvaluator()
+
+    evaluator = SpeculativeDecoderEvaluator(base_model=args.model)
     results = evaluator.evaluate(num_draft_tokens_list)
 
     plotter = ResultPlotter(num_draft_tokens_list, results)
